60a8d0fbc87b4bf2e1cd7a264ff12de7e8f4a9ef
Mar1an1 <marian.polacek@outlook.sk>
2023-10-16 20:21:54 +0200

    modif repositories

--- EbayClone/src/main/java/com/dev/mpolacek/ebayclone/repositories/ItemRepository.java
+++ EbayClone/src/main/java/com/dev/mpolacek/ebayclone/repositories/ItemRepository.java
@@ -1,10 +1,18 @@
 package com.dev.mpolacek.ebayclone.repositories;
 
 import com.dev.mpolacek.ebayclone.models.Item;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
     boolean existsById(Long id);
+
+    Page<Item> findBySellerUsernameAndDeletedIsFalse(Long userId, Pageable pageable);
+
+    Optional<Item> findByIdAndDeletedIsFalse(Long itemId);
 }
--- EbayClone/src/main/java/com/dev/mpolacek/ebayclone/repositories/UserRepository.java
+++ EbayClone/src/main/java/com/dev/mpolacek/ebayclone/repositories/UserRepository.java
@@ -7,5 +7,9 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsById(Long id);
+
+    User findByUsername(String username);
+
+
 }
 
