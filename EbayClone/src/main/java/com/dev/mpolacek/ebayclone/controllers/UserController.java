203066fef4d4ffd5f762ca91c28302f8ec4c6cb4
Mar1an1 <marian.polacek@outlook.sk>
2023-10-23 22:44:02 +0200

    add user controller

---
+++ EbayClone/src/main/java/com/dev/mpolacek/ebayclone/controllers/UserController.java
@@ -0,0 +1,33 @@
+package com.dev.mpolacek.ebayclone.controllers;
+
+import com.dev.mpolacek.ebayclone.models.dtos.LoginDto;
+import com.dev.mpolacek.ebayclone.models.dtos.SignUpDto;
+import com.dev.mpolacek.ebayclone.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<SignUpDto> signUp(@RequestBody SignUpDto signUpDto) {
+        return ResponseEntity.ok(userService.signUp(signUpDto));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginDto> login(@RequestBody LoginDto loginDto) {
+        return ResponseEntity.ok(userService.login(loginDto));
+    }
+}
