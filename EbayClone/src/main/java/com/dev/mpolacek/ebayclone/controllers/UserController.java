40482f82ca5e63550d815ca359f67ad547e7292f
Mar1an1 <marian.polacek@outlook.sk>
2023-10-25 20:36:13 +0200

    add endpoints to UserController

--- EbayClone/src/main/java/com/dev/mpolacek/ebayclone/controllers/UserController.java
+++ EbayClone/src/main/java/com/dev/mpolacek/ebayclone/controllers/UserController.java
@@ -1,14 +1,17 @@
 package com.dev.mpolacek.ebayclone.controllers;
 
+import com.dev.mpolacek.ebayclone.models.Item;
+import com.dev.mpolacek.ebayclone.models.User;
 import com.dev.mpolacek.ebayclone.models.dtos.LoginDto;
 import com.dev.mpolacek.ebayclone.models.dtos.SignUpDto;
 import com.dev.mpolacek.ebayclone.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/users")
@@ -21,5 +24,45 @@ public class UserController {
         this.userService = userService;
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getUserProfile(@PathVariable Long id) {
+        Optional<User> user = userService.getUserById(id);
 
+        if (user.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+        }
+
+        return ResponseEntity.ok(user.get());
+    }
+
+    @PutMapping("/{id}/update")
+    public ResponseEntity<?> updateUserProfile(@PathVariable Long id, @RequestBody User updatedUser) {
+        Optional<User> user = userService.getUserById(id);
+
+        if (user.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+        }
+
+        // Update user profile with the provided data
+        user.get().setUsername(updatedUser.getUsername());
+        user.get().setEmail(updatedUser.getEmail());
+
+        // Save the updated user
+        userService.save(user.get());
+
+        return ResponseEntity.ok(user.get());
+    }
+
+    @GetMapping("/{id}/products")
+    public ResponseEntity<?> getUserProducts(@PathVariable Long id) {
+        Optional<User> user = userService.getUserById(id);
+
+        if (user.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+        }
+
+        List<Item> products = user.get().getItems();
+
+        return ResponseEntity.ok(products);
+    }
 }
